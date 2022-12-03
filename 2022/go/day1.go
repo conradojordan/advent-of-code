ca9386c3f998ee94a6767f4fd59d98fe926ea55a
Conrado Jordan <conrado.jordan@gmail.com>
2022-12-03 16:25:41 -0300

    Add day 1 part 1 and 2

--- 2022/go/day1.go
+++ 2022/go/day1.go
@@ -9,7 +9,7 @@ import (
 
 const FilePath = "2022/inputs/day1"
 
-func check(e error) {
+func checkError(e error) {
 	if e != nil {
 		panic(e)
 	}
@@ -25,26 +25,45 @@ func countCalories(elf string) int {
 			continue
 		}
 		calories, err := strconv.Atoi(food)
-		check(err)
+		checkError(err)
 		caloriesCount += calories
 	}
 	return caloriesCount
 }
 
+func insertIntoTop3(top3 [3]int, newValue int) [3]int {
+	if newValue > top3[2] {
+		// Insert in 1st position
+		top3[0], top3[1], top3[2] = top3[1], top3[2], newValue
+	} else if newValue > top3[1] {
+		// Insert in 2nd position
+		top3[0], top3[1] = top3[1], newValue
+	} else {
+		// Insert in 3rd position
+		top3[0] = newValue
+	}
+	return top3
+}
+
 func main() {
 	data, err := os.ReadFile(FilePath)
-	check(err)
+	checkError(err)
 
 	stringData := string(data)
 	splittedData := strings.Split(stringData, "\n\n")
 
-	var maxCalories, currentCalories int
+	var top3calories [3]int
+	var currentCalories int
 	for _, elf := range splittedData {
 		currentCalories = countCalories(elf)
-		if currentCalories > maxCalories {
-			maxCalories = currentCalories
+		if currentCalories > top3calories[0] {
+			top3calories = insertIntoTop3(top3calories, currentCalories)
 		}
 	}
-	fmt.Printf("Elf with the highest amount of calories has: %v calories\n", maxCalories)
+	fmt.Printf("Top 3 most calories: %v\n", top3calories)
+	fmt.Printf("%sPart 1%s\n", strings.Repeat("-", 10), strings.Repeat("-", 10))
+	fmt.Printf("Elf with the highest amount of calories has: %v calories\n", top3calories[2])
+	fmt.Printf("%sPart 2%s\n", strings.Repeat("-", 10), strings.Repeat("-", 10))
+	fmt.Printf("Sum of the top 3 calories: %d\n", top3calories[0]+top3calories[1]+top3calories[2])
 
 }
