c1632e8f46c8f8c0a34617823ba71b2298512564
Conrado Jordan <conrado.jordan@gmail.com>
2022-12-18 20:17:08 -0300

    Add day 8 part 2

--- 2022/go/day8.go
+++ 2022/go/day8.go
@@ -65,7 +65,51 @@ func verticalVisibleTrees(forest []string, visibleTrees trees) trees {
 	return visibleTrees
 }
 
+func getHighestScenicScore(forest []string) (int, int, int) {
+	var maxScenicScore, scenicScore int
+	var maxI, maxJ int
 
+	for i := 1; i < len(forest)-1; i++ {
+		for j := 1; j < len(forest[i])-1; j++ {
+			scenicScore = getScenicScore(i, j, forest)
+			if scenicScore > maxScenicScore {
+				maxScenicScore = scenicScore
+				maxI, maxJ = i, j
+			}
+		}
+	}
+	return maxScenicScore, maxI, maxJ
+}
+
+func getScenicScore(i, j int, forest []string) int {
+	var up, down, left, right int
+
+	for k := i - 1; k >= 0; k-- {
+		up++
+		if forest[k][j] >= forest[i][j] {
+			break
+		}
+	}
+	for k := i + 1; k <= len(forest)-1; k++ {
+		down++
+		if forest[k][j] >= forest[i][j] {
+			break
+		}
+	}
+	for k := j - 1; k >= 0; k-- {
+		left++
+		if forest[i][k] >= forest[i][j] {
+			break
+		}
+	}
+	for k := j + 1; k <= len(forest[i])-1; k++ {
+		right++
+		if forest[i][k] >= forest[i][j] {
+			break
+		}
+	}
+	return up * down * left * right
+}
 
 func main() {
 	data, err := os.ReadFile(FilePath)
@@ -80,10 +124,12 @@ func main() {
 	visibleTrees = horizontalVisibleTrees(forest, visibleTrees)
 	visibleTrees = verticalVisibleTrees(forest, visibleTrees)
 
+	highestScenicScore, i, j := getHighestScenicScore(forest)
+
 	fmt.Printf("%sPart 1%s\n", strings.Repeat("-", 10), strings.Repeat("-", 10))
 	fmt.Println("Number of visible trees in the forest is:", len(visibleTrees))
-	// fmt.Printf("%sPart 2%s\n", strings.Repeat("-", 10), strings.Repeat("-", 10))
-	// fmt.Printf("Tree with highest scenic score has scenic score of: %d\n", part2answer)
+	fmt.Printf("%sPart 2%s\n", strings.Repeat("-", 10), strings.Repeat("-", 10))
+	fmt.Printf("Tree with highest scenic score has scenic score of: %d (found at coordinates {%d, %d})\n", highestScenicScore, i, j)
 
 	fmt.Printf("\nTotal time elapsed: %v\n", time.Since(startTime))
 }
