284dd84c18ea7ea8107b5a17d11230f2f737d919
Conrado Jordan <conrado.jordan@gmail.com>
2022-12-26 22:21:15 -0300

    Add day 9 part 2

--- 2022/go/day9.go
+++ 2022/go/day9.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"math"
 	"os"
 	"strconv"
 	"strings"
@@ -16,75 +17,77 @@ func checkError(e error) {
 	}
 }
 
-type Rope struct {
-	x, y        int
-	horiz, vert int8
+type Coordinates struct {
+	x, y int
+}
 
-	tailPositions map[string]bool
+func calculateNewCoord(currentCoord, pullingKnotCord Coordinates) Coordinates {
+	var newCoords Coordinates = currentCoord
+	dx, dy := pullingKnotCord.x-currentCoord.x, pullingKnotCord.y-currentCoord.y
+
+	if math.Abs(float64(dx)) == 2 {
+		newCoords.x += dx / 2
+		if math.Abs(float64(dy)) == 1 {
+			newCoords.y += dy
+		}
+	}
+	if math.Abs(float64(dy)) == 2 {
+		newCoords.y += dy / 2
+		if math.Abs(float64(dx)) == 1 {
+			newCoords.x += dx
+		}
+	}
+	return newCoords
+}
+
+type Rope struct {
+	coords [10]Coordinates
+
+	secondKnotPositions map[string]bool
+	tailPositions       map[string]bool
+}
+
+func (r *Rope) updateCoords() {
+	for i := 1; i < len(r.coords); i++ {
+		r.coords[i] = calculateNewCoord(r.coords[i], r.coords[i-1])
+	}
 }
 
 func (r *Rope) R(times int) {
 	for i := 0; i < times; i++ {
-		if r.horiz == 1 {
-			r.x++
-			if r.vert != 0 {
-				r.y += int(r.vert)
-				r.vert = 0
-			}
-			r.addTailPosition(r.x, r.y)
-		} else {
-			r.horiz++
-		}
+		r.coords[0].x++
+		r.updateCoords()
+		r.savePositions()
 	}
 }
 
 func (r *Rope) L(times int) {
 	for i := 0; i < times; i++ {
-		if r.horiz == -1 {
-			r.x--
-			if r.vert != 0 {
-				r.y += int(r.vert)
-				r.vert = 0
-			}
-			r.addTailPosition(r.x, r.y)
-		} else {
-			r.horiz--
-		}
+		r.coords[0].x--
+		r.updateCoords()
+		r.savePositions()
 	}
 }
 
 func (r *Rope) U(times int) {
 	for i := 0; i < times; i++ {
-		if r.vert == 1 {
-			r.y++
-			if r.horiz != 0 {
-				r.x += int(r.horiz)
-				r.horiz = 0
-			}
-			r.addTailPosition(r.x, r.y)
-		} else {
-			r.vert++
-		}
+		r.coords[0].y++
+		r.updateCoords()
+		r.savePositions()
 	}
 }
 
 func (r *Rope) D(times int) {
 	for i := 0; i < times; i++ {
-		if r.vert == -1 {
-			r.y--
-			if r.horiz != 0 {
-				r.x += int(r.horiz)
-				r.horiz = 0
-			}
-			r.addTailPosition(r.x, r.y)
-		} else {
-			r.vert--
-		}
+		r.coords[0].y--
+		r.updateCoords()
+		r.savePositions()
 	}
 }
 
-func (r *Rope) addTailPosition(x, y int) {
-	r.tailPositions[fmt.Sprintf("(%d,%d)", x, y)] = true
+func (r *Rope) savePositions() {
+	r.tailPositions[fmt.Sprintf("(%d,%d)", r.coords[9].x, r.coords[9].y)] = true
+	r.secondKnotPositions[fmt.Sprintf("(%d,%d)", r.coords[1].x, r.coords[1].y)] = true
 }
 
 func main() {
@@ -96,9 +99,10 @@ func main() {
 	ropeMovements := strings.Split(stringData, "\n")
 
 	var rope Rope = Rope{
-		tailPositions: make(map[string]bool),
+		tailPositions:       make(map[string]bool),
+		secondKnotPositions: make(map[string]bool),
 	}
-	rope.addTailPosition(0, 0)
+	rope.savePositions() // Save coordinates (0,0)
 
 	funcMappings := map[string]func(int){
 		"R": rope.R,
@@ -120,9 +124,9 @@ func main() {
 	}
 
 	fmt.Printf("%sPart 1%s\n", strings.Repeat("-", 10), strings.Repeat("-", 10))
-	fmt.Println("Answer to part 1 is:", len(rope.tailPositions))
+	fmt.Println("Answer to part 1 is:", len(rope.secondKnotPositions))
 	fmt.Printf("%sPart 2%s\n", strings.Repeat("-", 10), strings.Repeat("-", 10))
-	// fmt.Println("Answer to part 2 is:", part2answer)
+	fmt.Println("Answer to part 2 is:", len(rope.tailPositions))
 
 	fmt.Printf("\nTotal time elapsed: %v\n", time.Since(startTime))
 }
